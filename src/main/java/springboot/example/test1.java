58a539d6991dc0c9f42795af74c2ba2c6f07fe60
xuejl <744196876@qq.com>
2017-02-14 10:59:29 +0800

    d

---
+++ src/main/java/springboot/example/test1.java
@@ -0,0 +1,10 @@
+package springboot.example;
+
+/**
+ * Created by xjl on 2017/2/14.
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
