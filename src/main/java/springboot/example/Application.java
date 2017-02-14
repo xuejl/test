83dea73ca2a60d966ded3b5357ed2de855ada9ea
xuejl <744196876@qq.com>
2017-02-14 09:15:02 +0800

    haha

--- src/main/java/springboot/example/Application.java
+++ src/main/java/springboot/example/Application.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
     @RequestMapping("/")
     String home(){
-        return "hello world";
+        return "hello world1";
     }
 
     public static void main(String[] args) {
