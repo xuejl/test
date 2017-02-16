99879e992c2cbde6f0252764b7ebd196a6c57ef4
xuejl <744196876@qq.com>
2017-02-16 13:35:15 +0800

    快递鸟物流轨迹即时查询接口

--- src/test/java/KdniaoTrackQueryAPI.java
+++ src/test/java/KdniaoTrackQueryAPI.java
@@ -10,19 +10,7 @@ import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *
- * 快递鸟物流轨迹即时查询接口
- *
- * @技术QQ群: 456320272
- * @see: http://www.kdniao.com/YundanChaxunAPI.aspx
- * @copyright: 深圳市快金数据技术服务有限公司
- *
- * DEMO中的电商ID与私钥仅限测试使用，正式环境请单独注册账号
- * 单日超过500单查询量，建议接入我方物流轨迹订阅推送接口
- *
- * ID和Key请到官网申请：http://www.kdniao.com/ServiceApply.aspx
- */
+
 public class KdniaoTrackQueryAPI {
 
     //DEMO
